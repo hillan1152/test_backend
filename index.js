0c550f0aaa129f4e2fa549465ef61fdf1263d8a7
Anthony Hill <hillan1152@gmail.com>
2025-04-08 21:08:32 -0400

    added knex file

---
+++ index.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+const server = require('./server.js');
+
+const PORT = process.env.PORT || 8000;
+
+server.listen(PORT, () => {
+    console.log(`\n=== Server listening on port ${PORT} ===\n`);
+});
